5e0b35d9a4d20df3e2d36f360297d9a5680ff865
dio cancaro è la mia bestemmia preferita <luca.lovi.369@gmail.com>
2026-09-24 01:54:07 +0200

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -19,6 +19,10 @@ export default {
                 <div class="type-title-sm">Creator & Verifier</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
+                     </p>
+                <div class="type-title-sm">Verifier</div>
+                <p class="type-body">
+                    <span>{{ verifier }}</span>
                 </p>
             </template>
             <template v-else-if="creators.length === 0">
@@ -34,10 +38,6 @@ export default {
                         <span >{{ creator }}</span
                         ><span v-if="index < creators.length - 1">, </span>
                     </template>
-                </p>
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
                 </p>
             </template>
         </div>
